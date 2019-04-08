2bb0ada6870e0aec213b274eb67efee3e662aad7
Ryan Silva <ryandotsilva@gmail.com>
2019-04-08 19:20:18 -0400

    Change burgerIngredient to class component

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,37 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './BurgerIngredient.module.css';
 
-const burgerIngredient = props => {
-	let ingredient = null;
-
-	switch (props.type) {
-		case 'bread-bottom':
-			ingredient = <div className={styles.BreadBottom} />;
-			break;
-		case 'bread-top':
-			ingredient = (
-				<div className={styles.BreadTop}>
-					<div className={styles.Seeds1} />
-					<div className={styles.Seeds2} />
-				</div>
-			);
-			break;
-		case 'meat':
-			ingredient = <div className={styles.Meat} />;
-			break;
-		case 'cheese':
-			ingredient = <div className={styles.Cheese} />;
-			break;
-		case 'bacon':
-			ingredient = <div className={styles.Baco} />;
-			break;
-		case 'salad':
-			ingredient = <div className={styles.Salad} />;
-			break;
-		default:
-			ingredient = null;
+class BurgerIngredient extends Component {
+	render() {
+		switch (this.props.type) {
+			case 'bread-bottom':
+				ingredient = <div className={styles.BreadBottom} />;
+				break;
+			case 'bread-top':
+				ingredient = (
+					<div className={styles.BreadTop}>
+						<div className={styles.Seeds1} />
+						<div className={styles.Seeds2} />
+					</div>
+				);
+				break;
+			case 'meat':
+				ingredient = <div className={styles.Meat} />;
+				break;
+			case 'cheese':
+				ingredient = <div className={styles.Cheese} />;
+				break;
+			case 'bacon':
+				ingredient = <div className={styles.Baco} />;
+				break;
+			case 'salad':
+				ingredient = <div className={styles.Salad} />;
+				break;
+			default:
+				ingredient = null;
+		}
+		return ingredient;
 	}
-	return ingredient;
-};
+}
 
 export default burgerIngredient;
