d3f29e94d5862655696b019629564e08449fdec8
Ryan Silva <ryandotsilva@gmail.com>
2019-04-09 17:25:38 -0400

    Add reduce method to transformedIngredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-	const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-		return [...Array(props.ingredients[igKey])].map((_, i) => {
-			return <BurgerIngredient key={igKey + i} type={igKey} />;
-		});
-	});
+	const transformedIngredients = Object.keys(props.ingredients)
+		.map(igKey => {
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey} />;
+			});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el);
+		}, []);
+	console.log(transformedIngredients);
 	return (
 		<div className={styles.Burger}>
 			<BurgerIngredient type="bread-top" />
