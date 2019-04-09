233d73aa428cb0792bcff0b967b327701df8f5d8
Ryan Silva <ryandotsilva@gmail.com>
2019-04-09 17:27:19 -0400

    Add message to add ingredients if none

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,7 +3,7 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = props => {
-	const transformedIngredients = Object.keys(props.ingredients)
+	let transformedIngredients = Object.keys(props.ingredients)
 		.map(igKey => {
 			return [...Array(props.ingredients[igKey])].map((_, i) => {
 				return <BurgerIngredient key={igKey + i} type={igKey} />;
@@ -12,7 +12,9 @@ const burger = props => {
 		.reduce((arr, el) => {
 			return arr.concat(el);
 		}, []);
-	console.log(transformedIngredients);
+	if (transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please start adding ingredits!</p>;
+	}
 	return (
 		<div className={styles.Burger}>
 			<BurgerIngredient type="bread-top" />
