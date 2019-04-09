752e565b4fa1460f2f9cc6f40c4d13d71957d210
Ryan Silva <ryandotsilva@gmail.com>
2019-04-09 19:20:14 -0400

    Add OrderSummary.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,6 +83,7 @@ class BurgerBuilder extends Component {
 		}
 		return (
 			<React.Fragment>
+				<Modal />
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls
 					ingredientAdded={this.addIngredientHandler}
