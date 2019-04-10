fa4bed43ca879ced92cb0a4657f0b4dabee6ffee
Ryan Silva <ryandotsilva@gmail.com>
2019-04-10 17:23:44 -0400

    Add perchaseCancelHandler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -80,6 +80,10 @@ class BurgerBuilder extends Component {
 		this.setState({ purchasing: true });
 	};
 
+	purchaseCancelHandler = () => {
+		this.setState({ purchasing: false });
+	};
+
 	render() {
 		const disabledInfo = {
 			...this.state.ingredients
@@ -89,7 +93,10 @@ class BurgerBuilder extends Component {
 		}
 		return (
 			<React.Fragment>
-				<Modal show={this.state.purchasing}>
+				<Modal
+					show={this.state.purchasing}
+					modalClosed={this.purchaseCancelHandler}
+				>
 					<OrderSummary ingredients={this.state.ingredients} />
 				</Modal>
 				<Burger ingredients={this.state.ingredients} />
