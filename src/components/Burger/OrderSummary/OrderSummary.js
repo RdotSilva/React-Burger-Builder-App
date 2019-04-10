1327a409ad47dd7b7696823122ebdafe0abee940
Ryan Silva <ryandotsilva@gmail.com>
2019-04-10 17:35:34 -0400

    Replace old button with Button component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = props => {
 	const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
@@ -15,8 +16,8 @@ const orderSummary = props => {
 			<p>A delicious burger with the following ingredients:</p>
 			<ul>{ingredientSummary}</ul>
 			<p>Continue to Checkout?</p>
-			<button>CANCEL</button>
-			<button>CONTINUE</button>
+			<Button>CANCEL</Button>
+			<Button>CONTINUE</Button>
 		</React.Fragment>
 	);
 };
