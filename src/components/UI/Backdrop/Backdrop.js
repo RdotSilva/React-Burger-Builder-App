0aac5cde944e1611791b03db45636e72ba0832b8
Ryan Silva <ryandotsilva@gmail.com>
2019-04-10 17:19:49 -0400

    Add React.Fragment wrapper

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import styles from './Backdrop.module.css';
 
-
-const backdrop = props => (
-    props.show ? <div className={styles.Backdrop}></div> : null;
-);
+const backdrop = props =>
+	props.show ? <div className={styles.Backdrop} /> : null;
 
 export default backdrop;
