6ebe1424e23bc12d277fa9405f1b307b2c66311d
Ryan Silva <ryandotsilva@gmail.com>
2019-04-11 19:07:38 -0400

    Import styles and assign

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,7 +1,8 @@
 import React from 'React';
+import styles from './NavigationItem.module.css';
 
 const navigationItem = props => (
-	<li>
+	<li className={styles.NavigationItem}>
 		<a href="/">A link</a>
 	</li>
 );
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './NavigationItems.module.css';
 
 const navigationItems = () => (
-	<ul>
+	<ul className={styles.NavigationItems}>
 		<li>
 			<a href="/">A link</a>
 		</li>
