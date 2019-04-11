d1af1dcb19cb0bd5b7a5a342b222a58193143061
Ryan Silva <ryandotsilva@gmail.com>
2019-04-11 19:11:13 -0400

    Add Burger and Checkout to Nav

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,7 +3,9 @@ import styles from './NavigationItem.module.css';
 
 const navigationItem = props => (
 	<li className={styles.NavigationItem}>
-		<a href="/">A link</a>
+		<a href={props.link} className={props.active ? styles.active : null}>
+			{props.children}
+		</a>
 	</li>
 );
 
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './NavigationItems.module.css';
+import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
 	<ul className={styles.NavigationItems}>
 		<li>
-			<a href="/">A link</a>
+			<NavigationItem link="/" active>
+				Burger Builder
+			</NavigationItem>
+			<NavigationItem link="/">Checkout</NavigationItem>
 		</li>
 	</ul>
 );
