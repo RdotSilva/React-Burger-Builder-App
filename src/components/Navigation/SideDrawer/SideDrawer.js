4b4df94ee032c035df193bf8705f22e01a8f121f
Ryan Silva <ryandotsilva@gmail.com>
2019-04-11 20:50:39 -0400

    Add attachedClasses to hide sidedrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -5,11 +5,16 @@ import styles from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = props => {
-	//...
+	let attachedClasses = [styles.SideDrawer, styles.Close];
+
+	if (props.open) {
+		attachedClasses = [styles.SideDrawer, styles.Open];
+	}
+
 	return (
 		<React.Fragment>
 			<Backdrop show={props.open} clicked={props.closed} />
-			<div className={styles.SideDrawer}>
+			<div className={attachedClasses.join(' ')}>
 				<div className={styles.Logo}>
 					<Logo />
 				</div>
